1e3c636ae37dabdc7609c420e11f513e4caf3e90
Roman-code-web <selenerg.275@gmail.com>
2023-01-17 09:52:38 -0500

    usuarios ya se ve en el template

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserdataService } from 'src/app/servicios/userdata.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  //1.declaro variables
+  listaUser:any=[];
+  nombreApp!:string;
+  //2.
+  constructor( private userdataservice:UserdataService){}
 
+  //4.
+  ngOnInit(): void {
+    this.getlistarUser();
+    this.nombreApp= this.userdataservice.nombreAPP;
+  }
+
+  //3.
+  getlistarUser(){
+    this.userdataservice.getUser().subscribe(
+      res=>{
+        this.listaUser=res
+      },
+      error=>{}
+    )
+  }
 }
--- src/app/servicios/userdata.service.ts
+++ src/app/servicios/userdata.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserdataService {
+  //1. declaro variable 
+  urlUser="https://jsonplaceholder.typicode.com/users";
+  nombreAPP="Lista de usuarios con Interceptor"
+  //2. Constructor
+  constructor( private http:HttpClient) { }
 
-  constructor() { }
+  //3. funcion
+  getUser(){
+    return this.http.get<any>(this.urlUser);
+  }
+
 }
